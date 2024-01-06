5772f6b6f8ad10736aff93533cdcc10df778c4db
Almir Ramos <ardsdev23@gmail.com>
2024-01-06 07:00:33 -0300

    Nova activity_detalhes.xml

---
+++ app/src/main/java/com/example/projetogithub/DetalhesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projetogithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+    }
+}
